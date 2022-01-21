339581c92910ead6187da38d4e67973bfd665f3d
Erbin Rodriguez <erbin.rodriguez@acin.pt>
2022-01-21 13:31:54 +0000

    change title

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,7 +12,7 @@
     <script src="<?php echo base_url(); ?>assets/js/weatherAPI.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/deals.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
-    <title>Learning...</title>
+    <title>Learning.....</title>
 </head>
 
 <body class="h-100">
